048d449a5a09458f05e35c874272233ea8ae6f89
neodyma <neodyma@users.noreply.github.com>
2025-10-30 14:57:30 +0100

    set up configs

---
+++ frontend/.prettierrc.mjs
@@ -0,0 +1,10 @@
+const config = {
+  bracketSpacing: true,
+  plugins: ["prettier-plugin-tailwindcss"],
+  printWidth: 100,
+  semi: false,
+  singleQuote: false,
+  tabWidth: 2,
+}
+
+export default config
---
+++ frontend/eslint.config.mjs
@@ -0,0 +1,13 @@
+import nextVitals from "eslint-config-next/core-web-vitals";
+import nextTs from "eslint-config-next/typescript";
+import perfectionist from "eslint-plugin-perfectionist";
+import { defineConfig, globalIgnores } from "eslint/config";
+
+const eslintConfig = defineConfig([
+  ...nextVitals,
+  ...nextTs,
+  globalIgnores([".next/**", "out/**", "build/**", "next-env.d.ts"]),
+  perfectionist.configs["recommended-natural"],
+]);
+
+export default eslintConfig;
