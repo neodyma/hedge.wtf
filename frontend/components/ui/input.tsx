52848802615c4ec07d66c118ca91434b87801681
neodyma <neodyma@users.noreply.github.com>
2025-10-30 23:00:08 +0100

    input styling

--- frontend/components/ui/input.tsx
+++ frontend/components/ui/input.tsx
@@ -5,14 +5,14 @@ import { cn } from "@/lib/utils"
 function Input({ className, type, ...props }: React.ComponentProps<"input">) {
   return (
     <input
-      type={type}
-      data-slot="input"
       className={cn(
-        "file:text-foreground placeholder:text-muted-foreground selection:bg-primary selection:text-primary-foreground dark:bg-input/30 h-10 w-full min-w-0 rounded-xs border-2 border-foreground/20 bg-background/60 px-3 py-2 text-base shadow-xs transition-[border,color] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
+        "file:text-foreground placeholder:text-muted-foreground selection:bg-primary selection:text-primary-foreground dark:bg-input/30 border-foreground/20 bg-background/60 h-10 w-full min-w-0 rounded-xs border-2 px-3 py-2 text-base shadow-xs transition-[border,color] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
         "focus-visible:border-foreground focus-visible:ring-0",
         "aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
-        className
+        className,
       )}
+      data-slot="input"
+      type={type}
       {...props}
     />
   )
